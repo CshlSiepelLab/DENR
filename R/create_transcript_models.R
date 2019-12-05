8c288906727ad972917f557e1a59183e2a0a5e4e
Noah Dukler <ned36@cornell.edu>
2019-12-05 15:47:54 -0500

    Fixed bug where mapply would simplify output
    Also swapped columns and rows in models
    Restored commented code

--- R/create_transcript_models.R
+++ R/create_transcript_models.R
@@ -9,7 +9,7 @@
 #' each element in the list contains the bins for each corresponding group of
 #' transcripts
 #' @return A list of matrices where each row in the matrix corresponds to a
-#' transcript and each column is a bin
+#' bin and each column is a transcript
 #' @export
 create_transcript_models <- function(transcript_groups, bins) {
   # check input class
@@ -23,24 +23,26 @@ create_transcript_models <- function(transcript_groups, bins) {
     stop("transcript_groups and bins should be lists of the same length")
   }
   # Compute percent overlap of each transcript per bin and cast to matrix
-  tx_matrix_models <- mapply(FUN = function(tx_grp, bins) {
+  tx_matrix_models <- mapply(FUN = function(tx_grp, group_bins) {
     # Get which transcripts intersect which bins
-    hits <- GenomicRanges::findOverlaps(bins, tx_grp)
+    hits <- GenomicRanges::findOverlaps(group_bins, tx_grp)
     # Compute number of bases the intersect the trancript per bin
-    overlaps <- GenomicRanges::pintersect(bins[S4Vectors::queryHits(hits)],
-                           tx_grp[S4Vectors::subjectHits(hits)])
+    overlaps <- GenomicRanges::pintersect(
+      group_bins[S4Vectors::queryHits(hits)],
+      tx_grp[S4Vectors::subjectHits(hits)]
+    )
     # Compute percent overlap
     percent_overlap <- IRanges::width(overlaps) /
-                        IRanges::width(bins[S4Vectors::queryHits(hits)])
+                        IRanges::width(group_bins[S4Vectors::queryHits(hits)])
     # Reshape into matrix
     po_matrix <- reshape2::acast(
         cbind(S4Vectors::as.data.frame(hits), percent_overlap),
-        formula = subjectHits ~ queryHits,
+        formula = queryHits ~ subjectHits,
         value.var = "percent_overlap",
         fill = 0
     )
-    rownames(po_matrix) <- tx_grp$TXNAME
+    colnames(po_matrix) <- tx_grp$TXNAME
     return(po_matrix)
-  }, tx_grp = transcript_groups, bins = bins)
+  }, tx_grp = transcript_groups, group_bins = bins, SIMPLIFY = F)
   return(tx_matrix_models)
 }
