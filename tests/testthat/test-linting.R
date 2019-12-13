0e8bac5696f161797ecfe00c3746ee9087fb1409
Noah Dukler <ned36@cornell.edu>
2019-12-13 16:21:23 -0500

    Start of vignette
    More will be added as package is developed

--- tests/testthat/test-linting.R
+++ tests/testthat/test-linting.R
@@ -3,8 +3,23 @@ testthat::test_that("Style should be lint-free", {
         requireNamespace("lintr", quietly = TRUE),
         message = "Package lintr must be installed!"
     )
+    # Create list of excluded files
+    excluded_files <- c(
+        list.files(file.path(lint_path, "data"),
+                   recursive = TRUE, full.names = TRUE),
+        list.files(file.path(lint_path, "doc"),
+                   recursive = TRUE, full.names = TRUE),
+        list.files(file.path(lint_path, "inst/doc"),
+                   recursive = TRUE, full.names = TRUE),
+        list.files(file.path(lint_path, "man"),
+                   recursive = TRUE, full.names = TRUE),
+        list.files(file.path(lint_path, "vignettes"),
+                   recursive = TRUE, full.names = TRUE)
+    )
+    # Check for lint
     lintr::expect_lint_free(
         path = lint_path,
-        relative_path = TRUE
-        )
+        relative_path = TRUE,
+        exclusions = excluded_files
+    )
 })
