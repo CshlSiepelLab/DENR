3a8bb8367dc2a6b142479d018684dc451d529576
Noah Dukler <ned36@cornell.edu>
2020-02-06 12:08:08 -0500

    Reworked the error function to have multiple options
    Error function now defaults to fitting using the log data. Also fixed a bug where the data was not being masked properly.

--- R/model_fitting.R
+++ R/model_fitting.R
@@ -1,3 +1,18 @@
+#' @title Mask data
+#'
+#' @description masks positions in data vector at indices specified in masks
+#' vector by replacing them with zeros.
+#'
+#' @param data a data vector
+#' @param masks a vector of masked indicies
+#'
+#' @name mask_data
+#' @rdname mask_data
+mask_data <- function(data, masks) {
+  data[masks] <- 0
+  return(data)
+}
+
 #' @title Sum of squares
 #'
 #' @description Computes sum-of-squares for a
@@ -6,16 +21,23 @@
 #' @param models the matrix of transcript models
 #' @param data a vector of the observed data
 #' @param lambda the weight of the lasso penalty
+#' @param transform how to transform the data (either "identity" or "log")
 #'
-#' @name sum_squares
-#' @rdname sum_squares
-sum_squares_lasso <- function(x, models, data, lambda = 0) {
+#' @name sum_squares_lasso
+#' @rdname sum_squares_lasso
+sum_squares_lasso <- function(x, models, data, lambda = 0,
+                              transform) {
   # Normalize the sum-of-squares by the length of the region so the
   # lasso penalty is comparable across regions of different length
-  return(
-    mean((data - (models %*% x))^2) +
-      lambda * sum(abs(x))
-  )
+  if (transform == "identity") {
+      ss <- mean((data - (models %*% x))^2) + lambda * sum(abs(x))
+  } else if (transform == "log") {
+      ss <- mean((log(data + 1e-3) - log((models %*% x) + 1e-3))^2) +
+        lambda * sum(abs(x))
+  } else {
+    stop("Invalid transform specified")
+  }
+  return(ss)
 }
 
 #' @title Fit model
@@ -25,7 +47,7 @@ sum_squares_lasso <- function(x, models, data, lambda = 0) {
 #'
 #' @param threads number of threads to use in model fitting
 #' @inheritParams add_data
-#' @inheritParams sum_squares
+#' @inheritParams sum_squares_lasso
 #'
 #' @include transcript_quantifier-class.R
 #' @name fit
@@ -33,23 +55,37 @@ sum_squares_lasso <- function(x, models, data, lambda = 0) {
 #'
 #' @export
 methods::setGeneric("fit",
-                    function(transcript_quantifier, lambda = 0, threads = 1) {
+                    function(transcript_quantifier, lambda = 0, threads = 1,
+                             transform = "log") {
                       standardGeneric("fit")
                     })
 
 #' @rdname fit
 methods::setMethod("fit",
   signature(transcript_quantifier = "transcript_quantifier"),
-  function(transcript_quantifier, lambda = 0, threads = 1) {
+  function(transcript_quantifier, lambda = 0, threads = 1,
+           transform = "log") {
     if (lambda < 0) {
       stop("lambda must be positive")
     }
+
+    # Handle transform option
+    transform_opts <- c("identity", "log")
+    if (length(transform > 1)) {
+      warning("Multiple transform options specified, using the first")
+      transform <- transform[1]
+    }
+    if (!transform %in% transform_opts) {
+      stop(paste("transform must be one of these:",
+           paste(transform_opts, collapse = ", ")))
+    }
+
     # Alias for ease of use
     tq <- transcript_quantifier
     # Zip together models, counts, and abundances
-    sufficient_values <- mapply(function(x, y, z) {
-      list(abundance = x, models = y, counts = z)
-    }, tq@model_abundance, tq@models, tq@counts, SIMPLIFY = FALSE)
+    sufficient_values <- mapply(function(x, y, z, za) {
+      list(abundance = x, models = y, counts = z, masks = za)
+    }, tq@model_abundance, tq@models, tq@counts, tq@masks, SIMPLIFY = FALSE)
     # Create iterator over list of abundances
     sv_iter <- iterators::iter(sufficient_values)
 
@@ -65,8 +101,9 @@ methods::setMethod("fit",
     estim <- foreach::foreach(sv = sv_iter) %doloop% {
       opt_result <- stats::optim(sv$abundance, fn = sum_squares_lasso,
                           models = sv$models,
-                          data = sv$counts,
+                          data = mask_data(sv$counts, sv$masks),
                           lambda = lambda,
+                          transform = transform,
                           lower = rep(0, length(sv$abundance)),
                           method = "L-BFGS-B")
       abundance_est <- opt_result$par
