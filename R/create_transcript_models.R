9dd4eb74cb455fc1de57e5516d18d831f57e22f8
Unknown <ned36@cornell.edu>
2020-03-25 11:18:21 -0400

    Speed up model construction
    Use all-vs-all strategy combined with more efficient allocation to speed up model construction ~4X and reduce memory footprint. Also had to enforce minimum distance >= bin_size for this to work which makes more sense anyway.

--- R/create_transcript_models.R
+++ R/create_transcript_models.R
@@ -2,50 +2,48 @@
 #'
 #' Function for generating binned transcript models given a set of bins
 #'
-#' @param transcript_groups A \code{\link[GenomicRanges]{GRangesList-class}}
-#' object, each element in the list contains transcripts from one gene or
-#' several genes which are overlapped.
+#' @param transcripts A \code{\link[GenomicRanges]{GRanges-class}}
+#' object
 #' @param bins A \code{\link[GenomicRanges]{GRangesList-class}} object,
 #' each element in the list contains the bins for each corresponding group of
 #' transcripts
+#' @param bin_size width of bins
 #' @param transcript_name_column A string that indicates which column in the
 #' GRanges object contain the transcript names
 #' @return A list of matrices where each row in the matrix corresponds to a
 #' bin and each column is a transcript
 #' @export
-create_transcript_models <- function(transcript_groups, bins,
+create_transcript_models <- function(transcripts, bins, bin_size,
                                      transcript_name_column) {
   # check input class
-  if (!methods::is(transcript_groups, "GRangesList")) {
-    stop("transcript_groups is not a GRangesList object")
-  }
-  if (!methods::is(bins, "GRangesList")) {
-    stop("bins is not a GRangesList object")
-  }
-  if (length(transcript_groups) != length(bins)) {
-    stop("transcript_groups and bins should be lists of the same length")
+  if (!methods::is(transcripts, "GRanges")) {
+    stop("transcripts is not a GRanges object")
   }
   # Compute percent overlap of each transcript per bin and cast to matrix
-  tx_matrix_models <- mapply(FUN = function(tx_grp, group_bins) {
-    # Get intersection of all bins against all transcripts
-    overlaps <- IRanges::pintersect(
-      IRanges::IRanges(rep(GenomicRanges::start(group_bins), length(tx_grp)),
-                       width = GenomicRanges::width(group_bins)),
-      IRanges::IRanges(start = rep(GenomicRanges::start(tx_grp),
-                                   each = length(group_bins)),
-                       end = rep(GenomicRanges::end(tx_grp),
-                                 each = length(group_bins))),
-      resolve.empty = "start.x"
-    )
+  # First find all pairwise overlaps
+  ovr <- IRanges::findOverlapPairs(transcripts, bins)
+  # Compute bin percent overlap for all intersections
+  ovr_intersect <- IRanges::pintersect(ovr, drop.nohit.ranges = FALSE)
+  ovr_val <- GenomicRanges::width(ovr_intersect) / bin_size
 
-    # Compute percent overlap
-    percent_overlap <- matrix(IRanges::width(overlaps) /
-                                IRanges::width(group_bins),
-                              nrow = length(group_bins),
-                              byrow = FALSE)
-    colnames(percent_overlap) <-
-      S4Vectors::elementMetadata(tx_grp)[, transcript_name_column]
-    return(percent_overlap)
-  }, tx_grp = transcript_groups, group_bins = bins, SIMPLIFY = F)
+  # Compute the dimensions of model matrix for each loci
+  d <- S4Vectors::elementNROWS(ovr_val)
+  # Pre-extract transcript names for ease of access
+  transcript_names <-
+    GenomicRanges::values(transcripts)[, transcript_name_column]
+  # Reshape intersection output into matricies of the correct dimension
+  tx_matrix_models <- lapply(
+    S4Vectors::split(d, names(d))[names(bins)], function(x) {
+      # Retrive rows in transcripts that each intersection pair came from
+      lookup_ind <- which(names(ovr_val) == names(x[1]))
+      # Place into matrix of correct dimension
+      m <- matrix(data = BiocGenerics::unlist(ovr_val[lookup_ind]),
+                  nrow = x[1],
+                  ncol = length(x))
+      # Set column names to correct transcript names
+      colnames(m) <- transcript_names[lookup_ind]
+      return(m)
+    }
+  )
   return(tx_matrix_models)
 }
