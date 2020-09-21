2bae933e25dcb58fa8c56264e4414c78aae51d4a
Yixin Zhao <gzbyzyx@126.com>
2020-09-21 13:58:09 -0400

    Show legend only once in the abundance track

--- R/tq_visualization.R
+++ R/tq_visualization.R
@@ -192,19 +192,36 @@ plot_model <- function(tq,
                                  sel_tx_key$tx_name), "model_key_label"]
       rm(tx_key, sel_tx_key, same_model_key, model_key_label)
       # plot datatrack for abundance
+      i = 1
       for (s in unique(GenomicRanges::strand(abundance))) {
-          abundance_tracks[[as.character(s)]] <- Gviz::DataTrack(
-              abundance[GenomicRanges::strand(abundance) == s],
-              type = "histogram",
-              name = paste0("Predicted abundance (", s, ")"),
-              groups = colnames(
-                  S4Vectors::elementMetadata(
-                      abundance[GenomicRanges::strand(abundance) == s])),
-              legend = TRUE,
-              strand = s,
-              chromosome = chrom,
-              stackedBars = TRUE
-        )
+          if (i == 1) {
+              abundance_tracks[[as.character(s)]] <- Gviz::DataTrack(
+                  abundance[GenomicRanges::strand(abundance) == s],
+                  type = "histogram",
+                  name = paste0("Predicted abundance (", s, ")"),
+                  groups = colnames(
+                      S4Vectors::elementMetadata(
+                          abundance[GenomicRanges::strand(abundance) == s])),
+                  legend = TRUE,
+                  strand = s,
+                  chromosome = chrom,
+                  stackedBars = TRUE
+              )
+              i <- i + 1
+          } else {
+              abundance_tracks[[as.character(s)]] <- Gviz::DataTrack(
+                  abundance[GenomicRanges::strand(abundance) == s],
+                  type = "histogram",
+                  name = paste0("Predicted abundance (", s, ")"),
+                  groups = colnames(
+                      S4Vectors::elementMetadata(
+                          abundance[GenomicRanges::strand(abundance) == s])),
+                  legend = FALSE,
+                  strand = s,
+                  chromosome = chrom,
+                  stackedBars = TRUE
+              )
+          }
       }
       abundance_max <-
           ceiling(abs(max(as.matrix(S4Vectors::mcols(abundance)))))
